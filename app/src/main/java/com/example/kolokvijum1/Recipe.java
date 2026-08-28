814195b82e55c8ed4479adb59c82d379e91ce3d4
IvanaSuljak <163130669+IvanaSuljak@users.noreply.github.com>
2026-08-28 12:09:57 +0200

    Update Recipe.java

--- app/src/main/java/com/example/kolokvijum1/Recipe.java
+++ app/src/main/java/com/example/kolokvijum1/Recipe.java
@@ -1,1 +1,26 @@
+package com.example.kolokvijum1;
 
+public class Recipe {
+
+    private String naziv;
+    private int vremePripreme;
+    private boolean omiljeno;
+
+    public Recipe(String naziv, int vremePripreme, boolean omiljeno) {
+        this.naziv = naziv;
+        this.vremePripreme = vremePripreme;
+        this.omiljeno = omiljeno;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public int getVremePripreme() {
+        return vremePripreme;
+    }
+
+    public boolean isOmiljeno() {
+        return omiljeno;
+    }
+}
